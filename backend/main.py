76da0a2976a0bc1173edb914dc8378f3c1857887
New0Cody <ajinkyaparanjape17@gmail.com>
2026-08-18 16:48:19 +0530

    Allow production frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -33,8 +33,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
+        "http://127.0.0.1:5173",
+        "https://talentgraph-delta.vercel.app/"
+],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
